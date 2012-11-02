e7dd1d54ca79bf1aff84fead9fb65bb78d4da0de
Robert Xu <robxu9@gmail.com>
2012-11-01 21:29:55 -0400

    Don't attach Idle Quests to the regular main quest manager. This is done namely to prevent Idle Quests from stopping on player logout.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/com/theminequest/idle/Main.java
+++ src/main/java/com/theminequest/idle/Main.java
@@ -19,6 +19,11 @@
 package com.theminequest.idle;
 
 import java.io.File;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
@@ -28,22 +33,22 @@ import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.theminequest.MineQuest.API.CompleteStatus;
 import com.theminequest.MineQuest.API.Managers;
 import com.theminequest.MineQuest.API.Quest.Quest;
 import com.theminequest.MineQuest.API.Quest.QuestDetails;
 import com.theminequest.MineQuest.API.Utils.PropertiesFile;
 
 public class Main extends JavaPlugin implements Listener {
-
+	
 	public static final String PROPERTIES_NAME = "idle.properties";
 	private PropertiesFile properties;
+	private LinkedHashMap<String,List<Quest>> quests;
 	
 	@Override
 	public void onDisable() {
 		
 	}
-
+	
 	@Override
 	public void onEnable() {
 		if (getServer().getPluginManager().getPlugin("MineQuest") == null) {
@@ -60,6 +65,7 @@ public class Main extends JavaPlugin implements Listener {
 		for (World w : Bukkit.getWorlds()){
 			properties.getString(w.getName(), "/");
 		}
+		quests = new LinkedHashMap<String, List<Quest>>();
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 	
@@ -70,33 +76,54 @@ public class Main extends JavaPlugin implements Listener {
 		String questName = properties.getString(e.getPlayer().getWorld().getName(), "/");
 		if (questName.equals("/"))
 			return;
-		Quest q = Managers.getQuestManager().getMainWorldQuest(e.getPlayer().getName(), questName);
-		if (q==null){
-			QuestDetails d = Managers.getQuestManager().getDetails(questName);
-			if (d!=null)
-				Managers.getQuestManager().startQuest(d,e.getPlayer().getName()).startQuest();
-		}
+		tryStart(e.getPlayer().getName(),questName);
+
 	}
 	
 	@EventHandler
 	public void onPlayerChangedWorld(PlayerChangedWorldEvent e){
 		if (e.getPlayer().getWorld().getName().startsWith("mqinstance_"))
 			return;
-		String stopquest = properties.getString(e.getFrom().getName(),"/");
-		if (!stopquest.equals("/")) {
-			Quest q = Managers.getQuestManager().getMainWorldQuest(e.getPlayer().getName(), stopquest);
-			if (q != null)
-				q.finishQuest(CompleteStatus.CANCELED);
-		}
+//		String stopquest = properties.getString(e.getFrom().getName(),"/");
+//		if (!stopquest.equals("/")) {
+//			Quest q = Managers.getQuestManager().getMainWorldQuest(e.getPlayer().getName(), stopquest);
+//			if (q != null)
+//				q.finishQuest(CompleteStatus.CANCELED);
+//		}
 		String questName = properties.getString(e.getPlayer().getWorld().getName(), "/");
 		if (questName.equals("/"))
 			return;
-		Quest q = Managers.getQuestManager().getMainWorldQuest(e.getPlayer().getName(), questName);
-		if (q==null){
+		
+		tryStart(e.getPlayer().getName(),questName);
+	}
+	
+	private void tryStart(String playerName, String questName) {
+		if (!quests.containsKey(playerName))
+			quests.put(playerName,new ArrayList<Quest>());
+		
+		if (!hasQuest(playerName,questName)) {
 			QuestDetails d = Managers.getQuestManager().getDetails(questName);
-			if (d!=null)
-				Managers.getQuestManager().startQuest(d,e.getPlayer().getName()).startQuest();
+			if (d!=null) {
+				try {
+					Method m = com.theminequest.MineQuest.Quest.Quest.class.getMethod("newInstance", long.class, QuestDetails.class, String.class);
+					m.setAccessible(true);
+					Quest q = (Quest) m.invoke(null, -1,d,playerName);
+					quests.get(playerName).add(q);
+					q.startQuest();
+				} catch (Exception ex) {
+					Managers.log(Level.SEVERE, "[Idle] Could not start Idle Background Quest!");
+					ex.printStackTrace();
+				}
+			}
 		}
+	}
+	
+	private boolean hasQuest(String playerName, String questName) {
+		for (Quest q : quests.get(playerName)) {
+			if (q.getDetails().getProperty(QuestDetails.QUEST_NAME).equals(questName))
+				return true;
+		}
+		return false;
 	}
 	
 }
